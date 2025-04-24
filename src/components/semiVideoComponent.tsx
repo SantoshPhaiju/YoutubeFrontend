b95fd3ab2423ad48d51b4cbcd07ffd3c97ce0cab
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-04-24 16:58:13 +0545

    feat: update SemiVideo component layout and add CommentComponent with modal functionality

--- src/components/semiVideoComponent.tsx
+++ src/components/semiVideoComponent.tsx
@@ -3,16 +3,16 @@ import { MdVerified } from "react-icons/md";
 
 const SemiVideo = () => {
   return (
-    <div className="w-[100%] h-[100px] cursor-pointer flex justify-between items-center gap-2 mt-3">
+    <div className="w-[100%] h-[120px] xl:h-[100px] cursor-pointer flex justify-start xl:justify-between items-center gap-2 mt-3">
       <Image
         src={"/assets/thumb.jpg"}
         height={200}
         width={300}
-        className="h-full w-[44%] rounded-md object-cover"
+        className="h-full w-[200px] xl:w-[44%] rounded-md object-cover"
         alt="VideoComponent"
       />
 
-      <div className="details w-[56%] h-full flex gap-2">
+      <div className="details xl:w-[56%] h-full flex gap-2">
         <div className="flex flex-col gap-1 px-1">
           <div className="title text-[14px] text-black font-medium leading-normal">
            {" How to Make Money Using Google Maps in 2025 ($100 - $200 PER DAY) something".substring(0, 40) + "..."}
