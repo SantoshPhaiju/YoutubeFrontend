fbba35c3e731a04a01b93c8f34febb7e42fdf7e7
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-08-21 19:05:28 +0545

    chore: remove debug log from CommentComponent

--- src/components/comment/comment-component.tsx
+++ src/components/comment/comment-component.tsx
@@ -11,7 +11,7 @@ import { Button } from "../ui/button";
 
 const CommentComponent = () => {
   const [showCommentModal, setShowCommentModal] = useState(false);
-  console.log("something is cooking");
+  
   return (
     <>
       <div className="commentSection ">
