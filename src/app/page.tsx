51cdb1a2c44ebd1569583d345ee4a5c7d6221f72
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-01 16:40:24 +0545

    feat: fetch and display dynamic videos on home page, implement `timeAgo` utility for relative timestamps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,53 @@
 import HomePageClient from "@/components/home-page.client";
 import VideoComponent from "@/components/video-component";
+import {getAllHomePageVideos} from "@/services/auth/auth.service";
 
-export default function Home() {
+export default async function Home() {
+  const videos = await getAllHomePageVideos({});
+  console.log("myvideos", videos);
   return (
     <div>
       <HomePageClient />
 
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4 gap-6 px-4 pr-6">
-        <div className="col-span-1">
-          <VideoComponent />
-        </div>
-        <div className="col-span-1">
-          <VideoComponent />
-        </div>
-        <div className="col-span-1">
-          <VideoComponent />
-        </div>
-        <div className="col-span-1">
-          <VideoComponent />
-        </div>
-        <div className="col-span-1">
-          <VideoComponent />
-        </div>
-        <div className="col-span-1">
-          <VideoComponent />
-        </div>
-        <div className="col-span-1">
-          <VideoComponent />
-        </div>
+        {
+          videos.map((video, index) => {
+            return (
+                <div key={index} className="col-span-1">
+                  <VideoComponent
+                      thumbnail={video.thumbnail}
+                      videoTitle={video.title}
+                      videoViews={video.viewCount}
+                      videoDuration={video.duration * 60 * 60}
+                      videoOwnerName={video.owner.fullName}
+                      videoPublishedDate={video.createdAt}
+                      userAvatar={video.owner.avatar}
+                  />
+                </div>
+            )
+          })
+        }
+        {/*<div className="col-span-1">*/}
+        {/*  <VideoComponent />*/}
+        {/*</div>*/}
+        {/*<div className="col-span-1">*/}
+        {/*  <VideoComponent />*/}
+        {/*</div>*/}
+        {/*<div className="col-span-1">*/}
+        {/*  <VideoComponent />*/}
+        {/*</div>*/}
+        {/*<div className="col-span-1">*/}
+        {/*  <VideoComponent />*/}
+        {/*</div>*/}
+        {/*<div className="col-span-1">*/}
+        {/*  <VideoComponent />*/}
+        {/*</div>*/}
+        {/*<div className="col-span-1">*/}
+        {/*  <VideoComponent />*/}
+        {/*</div>*/}
+        {/*<div className="col-span-1">*/}
+        {/*  <VideoComponent />*/}
+        {/*</div>*/}
       </div>
     </div>
   );
