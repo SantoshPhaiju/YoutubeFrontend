d19621c1b36fcacafaf5b1ffa1e80e8a9b6121a3
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-05 19:18:11 +0545

    feat: extract video-related API calls to `video.service.ts` and remove redundant logic from `auth.service.ts`

---
+++ src/services/videos/video.service.ts
@@ -0,0 +1,40 @@
+// here we will write the backend communication logic, means sending http request to the backend
+
+import api from "@/services/axios";
+import {IVideo} from "@/@types/videos/videos.type";
+import {AxiosResponse} from "axios";
+
+export const getAllHomePageVideos = async ({}) => {
+    try {
+        const response = await api.get("/videos/get-home-page-videos");
+        return response.data.data.videos;
+    } catch (e) {
+        console.log("error", e);
+    }
+}
+
+type GetVideoParams = {
+    id: string;
+};
+
+type GetVideoResponse = {
+    data: {
+        video: IVideo;
+    };
+};
+
+export const getVideoById = async (
+    { id }: GetVideoParams
+): Promise<IVideo> => {
+    try {
+        const response: AxiosResponse<GetVideoResponse> =
+            await api.get(`/videos/get-video/${id}`);
+
+        console.log("response", response.data);
+
+        return response.data.data.video;
+    } catch (error) {
+        console.log("error", error);
+        throw error;
+    }
+};
