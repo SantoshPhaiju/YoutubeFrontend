f5381839c25367b278ac6a985e24b8d8123de185
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-05 19:19:18 +0545

    feat: integrate video data across pages, update layout and enhance APIs
    - Refactored video fetching APIs into `video.service.ts` for consistency.
    - Updated `Home` and `Watch` pages to use dynamic video data.
    - Improved video player and layout responsiveness on the watch page.
    - Enhanced metadata display (likes, views, subscribers) using fetched API data.
    - Replaced mock data with real-time video owner and description details.
    - Added `formatDate` and `timeAgo` utilities for improved date formatting.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
 import { IVideo } from "@/@types/videos/videos.type";
 import HomePageClient from "@/components/home-page.client";
 import VideoComponent from "@/components/video-component";
-import { getAllHomePageVideos } from "@/services/auth/auth.service";
+import {getAllHomePageVideos} from "@/services/videos/video.service";
 
 export default async function Home() {
   const videos: IVideo[] = await getAllHomePageVideos({});
 
-  console.log("myvideos", videos);
   return (
     <div>
       <HomePageClient />
-
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4 gap-6 px-4 pr-6">
         {videos?.map((video, index: number) => {
           return (
             <div key={index} className="col-span-1">
               <VideoComponent
+                  videoId={video._id}
                 thumbnail={video.thumbnail}
                 videoTitle={video.title}
                 videoViews={video.viewCount}
--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -1,5 +1,5 @@
-import CommentComponent from "@/components/comment/comment-component";
-import SeeMoreComponent from "@/components/seemore/see-more-component";
+import CommentComponent from "@/components/video/comment/comment-component";
+import SeeMoreComponent from "@/components/video/seemore/see-more-component";
 import SemiNav from "@/components/semi-nav";
 import SemiVideo from "@/components/semiVideoComponent";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -9,6 +9,9 @@ import Image from "next/image";
 import { BiDislike, BiLike } from "react-icons/bi";
 import { FiBookmark } from "react-icons/fi";
 import { PiShareFatLight } from "react-icons/pi";
+import {getVideoById} from "@/services/videos/video.service";
+import {timeAgo} from "@/utils/timeAgo";
+import {formatDate} from "@/utils/formatDate";
 
 interface WatchPageProps {
   searchParams: {
@@ -23,6 +26,12 @@ const Page = async ({ searchParams }: WatchPageProps) => {
     return <p>Invalid video ID</p>;
   }
 
+  const video = await getVideoById({ id: videoId });
+  console.log("video", video);
+
+  const videoOwner = Array.isArray(video.owner) ? video.owner[0]: video.owner;
+
+
   const desc =
     "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.";
   const videoDescription = desc.repeat(10);
@@ -32,31 +41,37 @@ const Page = async ({ searchParams }: WatchPageProps) => {
   return (
     <>
       <ClientVideoPageLayout>
-        <div className="px-4 md:px-[32px] lg:px-[48px] xl:px-[64px] pb-2 z-0">
+        <div className="px-4 md:px-8 lg:px-12 xl:px-16 pb-2 z-0">
           {/* <Videopage /> */}
           <div className="w-full px-2 mx-auto h-auto cursor-pointer flex flex-col xl:flex-row xl:justify-center xl:items-start gap-6">
-            <div className="leftContainer w-full flex flex-col gap-3">
-              <div className="videoPlayer w-full rounded-xl overflow-hidden">
-                <Image
-                  src={"/assets/thumb.jpg"}
-                  height={200}
-                  width={300}
-                  className="h-auto min-h-[200px] xl:h-auto xl:max-h-[70vh] w-full object-cover"
-                  alt="VideoComponent"
+            <div className="leftContainer w-full flex flex-col gap-3 xl:w-[70%] ">
+              <div className="videoPlayer w-full rounded-xl overflow-hidden aspect-video">
+                {/*<Image*/}
+                {/*  src={"/assets/thumb.jpg"}*/}
+                {/*  height={200}*/}
+                {/*  width={300}*/}
+                {/*  className="aspect-video w-full object-cover"*/}
+                {/*  alt="VideoComponent"*/}
+                {/*/>*/}
+                <video
+                    src={video.videoFile}
+                    controls
+                    autoPlay
+                    className="w-full h-full object-cover"
+                    loop
                 />
               </div>
               <div className="videoDetails flex flex-col gap-3">
                 <div className="text-lg md:text-xl font-bold leading-tight">
-                  Mitwa - Lyrical Song | KANK | Shahrukh Khan, Rani Mukherjee |
-                  Shankar Ehsaan Loy | Romantic Song
+                  {video?.title || "Video Title here..."}
                 </div>
                 <div className="flex gap-4 flex-col lg:flex-row justify-between">
                   <div className="channel flex gap-5 items-center justify-between xl:justify-start">
                     <div className="flex gap-2">
-                      <div className="w-[40px] h-[40px] z-0">
+                      <div className="w-10 h-10 z-0">
                         <Avatar className="">
                           <AvatarImage
-                            src="https://github.com/shadcn.png"
+                              src={videoOwner.avatar || `https://github.com/shadcn.png`}
                             alt="@shadcn"
                             className="rounded-[50%] z-0"
                           />
@@ -65,25 +80,34 @@ const Page = async ({ searchParams }: WatchPageProps) => {
                       </div>
                       <div className="flex flex-col">
                         <div className="text-lg font-semibold leading-tight font-roboto">
-                          Santosh Phaiju
+                          {videoOwner.fullname}
                         </div>
-                        <div className="text-[12px] text-gray-600">
-                          100K subscribers
+                        <div className="text-[12px] font-medium text-gray-600">
+                          {videoOwner.subscribersCount} subscribers
                         </div>
                       </div>
                     </div>
-                    <Button
-                      variant="default"
-                      className="rounded-full font-roboto font-medium"
+                    {!videoOwner.isSubscribed ? <Button
+                        variant="default"
+                        className="rounded-full font-roboto font-medium"
                     >
                       Subscribe
-                    </Button>
+                    </Button>: (
+                        <Button
+                            variant="ghost"
+                            className="rounded-full font-roboto font-medium"
+                        >
+                          Subscribed
+                        </Button>
+                    )}
                   </div>
                   <div className="flex justify-start xl:justify-end gap-1 md:gap-2 flex-wrap items-center ">
                     <div className="flex justify-between items-center rounded-full bg-gray-100">
                       <div className="flex justify-center border-r items-center rounded-l-full gap-1 md:gap-2 p-1.5 px-3 md:p-2 md:px-4 hover:bg-gray-200">
                         <BiLike className={"text-[16px] md:text-[20px]"} />
-                        <p className="font-sans font-semibold text-[12px] md:text-sm">50B</p>
+                        <p className="font-sans font-semibold text-[12px] md:text-sm">{
+                          video.likeCount
+                        }</p>
                       </div>
                       <div className="p-1.5 px-3 md:p-2 md:px-4 rounded-r-full hover:bg-gray-200">
                         <BiDislike className={"text-[16px] md:text-[20px]"} />
@@ -98,23 +122,27 @@ const Page = async ({ searchParams }: WatchPageProps) => {
                       <p className="font-sans font-semibold text-[12px] md:text-sm">Save</p>
                     </div>
                     <div className="rounded-full h-auto py-3.5 px-2 md:py-[18px] md:px-3 bg-gray-100 hover:bg-gray-200 flex justify-center gap-[2px] items-center">
-                      <div className="bg-gray-700 h-[3px] w-[3px] md:h-1 md:w-1 rounded-full"></div>
-                      <div className="bg-gray-700 h-[3px] w-[3px] md:h-1 md:w-1 rounded-full"></div>
-                      <div className="bg-gray-700 h-[3px] w-[3px] md:h-1 md:w-1 rounded-full"></div>
+                      <div className="bg-gray-700 h-0.75 w-0.75 md:h-1 md:w-1 rounded-full"></div>
+                      <div className="bg-gray-700 h-0.75 w-0.75 md:h-1 md:w-1 rounded-full"></div>
+                      <div className="bg-gray-700 h-0.75 w-0.75 md:h-1 md:w-1 rounded-full"></div>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="descriptionSection flex flex-col gap-2 py-[12px] px-[12px] h-auto w-full bg-gray-100 rounded-xl">
-                <div className="flex justify-start items-center gap-2 font-semibold text-sm font-roboto">
-                  <div>109, 447 views</div>
-                  <div>Feb 6, 2025</div>
+                <div className="flex justify-start items-center gap-2 font-semibold text-[15px] font-roboto">
+                  <div>{
+                    video.viewCount
+                  } views</div>
+                  <div>{
+                    formatDate(video.createdAt)
+                  }</div>
                 </div>
-                <SeeMoreComponent videoDescription={videoDescription} />
+                <SeeMoreComponent videoDescription={video.description} />
               </div>
               <CommentComponent />
             </div>
-            <div className="rightContainer w-full xl:block xl:w-[420px]">
+            <div className="rightContainer w-full xl:block xl:w-[30%] ">
               <SemiNav />
               <SemiVideo />
               <SemiVideo />
