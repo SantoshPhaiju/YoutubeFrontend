3348ab6e5f947e3e68e344de08910eff240c4b13
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-07 19:12:10 +0545

    refactor: clean up `SigninModal` and enhance dialog styles
    - Removed commented-out code and unused imports to improve readability.
    - Added max-height and overflow styling for better dialog usability and scrolling.
    - Adjusted button styles and added headings for better user experience in login/signup forms.
    - Updated `globals.css` with `.no-scrollbar` utility for cleaner scroll behavior.

--- src/components/navbar/signin-modal.tsx
+++ src/components/navbar/signin-modal.tsx
@@ -1,193 +1,3 @@
-// "use client";
-//
-// import React, {useState} from 'react';
-// import {FaRegCircleUser} from "react-icons/fa6";
-// import {Button} from "@/components/ui/button";
-//
-// import {
-//     Dialog, DialogClose,
-//     DialogContent,
-//     DialogDescription, DialogFooter,
-//     DialogHeader,
-//     DialogTitle,
-// } from "@/components/ui/dialog"
-// import {Controller, useForm} from "react-hook-form";
-// import {z} from "zod";
-// import {loginFormSchema} from "@/schemas/auth.schema";
-// import {zodResolver} from "@hookform/resolvers/zod";
-// import {Field, FieldError, FieldGroup, FieldLabel} from "@/components/ui/field";
-// import {Input} from "@/components/ui/input";
-// import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
-// import {signupFormSchema} from "@/schemas/auth.schema";
-//
-// const SigninModal = () => {
-//     const [open, setOpen] = useState(false);
-//     const loginForm = useForm<z.infer<typeof loginFormSchema>>({
-//         defaultValues: {
-//             email: "",
-//             password: "",
-//         },
-//         resolver: zodResolver(loginFormSchema),
-//     });
-//
-//     const signupForm = useForm<z.infer<typeof signupFormSchema>>({
-//         defaultValues: {
-//             username: "",
-//             fullname: "",
-//             email: "",
-//             password: "",
-//             confirmPassword: "",
-//             avatar: "",
-//         },
-//         resolver: zodResolver(signupFormSchema),
-//     })
-//
-//     const onLogin = (values: z.infer<typeof loginFormSchema>) => {
-//         console.log(values);
-//     }
-//
-//     const onSignup = (values: z.infer<typeof loginFormSchema>) => {
-//         console.log(values);
-//     }
-//
-//     return (
-//         <div>
-//             <Button
-//                 onClick={() => setOpen(true)}
-//                 variant={"outline"}
-//                 className={"rounded-full cursor-pointer font-normal text-[16px] px-2 py-2 text-blue-600 hover:text-blue-600 flex justify-center items-center gap-1.5"}>
-//                 <FaRegCircleUser className={"h-5! w-5!"}/>
-//                 Sign in
-//             </Button>
-//
-//             <Dialog open={open} onOpenChange={setOpen}>
-//                 <DialogContent className="[&>button]:hidden">
-//                     <DialogHeader className={"hidden"}>
-//                         <DialogTitle className={"font-inter hidden"}></DialogTitle>
-//                         <DialogDescription className={"hidden"}>
-//                             hello
-//                         </DialogDescription>
-//                     </DialogHeader>
-//                     <Tabs defaultValue="login" className="w-full">
-//                         <TabsList className={"w-full flex gap-2 mb-4!"}>
-//                             <TabsTrigger value="login" className={"w-full cursor-pointer"}>Login</TabsTrigger>
-//                             <TabsTrigger value="signup" className={"w-full cursor-pointer"}>Signup</TabsTrigger>
-//                         </TabsList>
-//                         <TabsContent value="login">
-//                             <form id={"form-login"} onSubmit={loginForm.handleSubmit(onLogin)}>
-//                                 <FieldGroup className={"gap-4"}>
-//                                     <Controller
-//                                         control={loginForm.control}
-//                                         name={"email"}
-//                                         render={({field, fieldState}) => (
-//                                             <Field data-invalid={fieldState.invalid}>
-//                                                 <FieldLabel htmlFor={field.name}>
-//                                                     Email Address
-//                                                 </FieldLabel>
-//                                                 <Input placeholder={"Enter your email"} {...field} id={field.name}
-//                                                        aria-invalid={fieldState.invalid}/>
-//                                                 {fieldState.invalid && (
-//                                                     <FieldError errors={[fieldState.error]}/>
-//                                                 )}
-//                                             </Field>
-//                                         )}
-//                                     />
-//
-//                                     <Controller
-//                                         control={loginForm.control}
-//                                         name={"password"}
-//                                         render={({field, fieldState}) => (
-//                                             <Field data-invalid={fieldState.invalid}>
-//                                                 <FieldLabel htmlFor={field.name}>
-//                                                     Password
-//                                                 </FieldLabel>
-//                                                 <Input placeholder={"Enter your password"} type={"password"} {...field}
-//                                                        id={field.name}
-//                                                        aria-invalid={fieldState.invalid}/>
-//                                                 {fieldState.invalid && (
-//                                                     <FieldError errors={[fieldState.error]}/>
-//                                                 )}
-//                                             </Field>
-//                                         )}
-//                                     />
-//                                 </FieldGroup>
-//                             </form>
-//                             <div className={"flex flex-col! gap-2 font-inter mt-4"}>
-//                                 <div className={"flex gap-2 w-full"}>
-//                                     <DialogClose className={"w-full"}>
-//                                         <Button variant={"outline"} className={"cursor-pointer w-full"}>
-//                                             Cancel
-//                                         </Button>
-//                                     </DialogClose>
-//                                     <Button form={"form-login"} type={"submit"} className={"cursor-pointer w-full"}>
-//                                         Login
-//                                     </Button>
-//                                 </div>
-//                             </div>
-//                         </TabsContent>
-//                         <TabsContent value="signup">
-//                             <form id={"form-signup"} onSubmit={signupForm.handleSubmit(onSignup)}>
-//                                 <FieldGroup className={"gap-4"}>
-//                                     <Controller
-//                                         control={signupForm.control}
-//                                         name={"email"}
-//                                         render={({field, fieldState}) => (
-//                                             <Field data-invalid={fieldState.invalid}>
-//                                                 <FieldLabel htmlFor={field.name}>
-//                                                     Email Address
-//                                                 </FieldLabel>
-//                                                 <Input placeholder={"Enter your email"} {...field} id={field.name}
-//                                                        aria-invalid={fieldState.invalid}/>
-//                                                 {fieldState.invalid && (
-//                                                     <FieldError errors={[fieldState.error]}/>
-//                                                 )}
-//                                             </Field>
-//                                         )}
-//                                     />
-//
-//                                     <Controller
-//                                         control={loginForm.control}
-//                                         name={"password"}
-//                                         render={({field, fieldState}) => (
-//                                             <Field data-invalid={fieldState.invalid}>
-//                                                 <FieldLabel htmlFor={field.name}>
-//                                                     Password
-//                                                 </FieldLabel>
-//                                                 <Input placeholder={"Enter your password"} type={"password"} {...field}
-//                                                        id={field.name}
-//                                                        aria-invalid={fieldState.invalid}/>
-//                                                 {fieldState.invalid && (
-//                                                     <FieldError errors={[fieldState.error]}/>
-//                                                 )}
-//                                             </Field>
-//                                         )}
-//                                     />
-//                                 </FieldGroup>
-//                             </form>
-//                             <div className={"flex flex-col! gap-2 font-inter mt-4"}>
-//                                 <div className={"flex gap-2 w-full"}>
-//                                     <DialogClose className={"w-full"}>
-//                                         <Button variant={"outline"} className={"cursor-pointer w-full"}>
-//                                             Cancel
-//                                         </Button>
-//                                     </DialogClose>
-//                                     <Button form={"form-signup"} type={"submit"} className={"cursor-pointer w-full"}>
-//                                         Signup
-//                                     </Button>
-//                                 </div>
-//                             </div>
-//                         </TabsContent>
-//                     </Tabs>
-//
-//                 </DialogContent>
-//             </Dialog>
-//         </div>
-//     );
-// };
-//
-// export default SigninModal;
-
-
 "use client";
 
 import React, { useState, useCallback } from "react";
@@ -272,14 +82,14 @@ const SigninModal = () => {
             <Button
                 onClick={() => setOpen(true)}
                 variant="outline"
-                className="rounded-full cursor-pointer font-normal text-[16px] px-2 py-2 text-blue-600 hover:text-blue-600 flex justify-center items-center gap-1.5"
+                className="rounded-full cursor-pointer font-medium text-[16px] px-2 py-2 text-blue-700 hover:text-blue-600 flex justify-center items-center gap-1.5 "
             >
                 <FaRegCircleUser className="h-5! w-5!" />
                 Sign in
             </Button>
 
             <Dialog open={open} onOpenChange={setOpen}>
-                <DialogContent className="[&>button]:hidden">
+                <DialogContent className="[&>button]:hidden border border-black overflow-y-auto max-h-[90vh]! no-scrollbar">
                     <DialogHeader className="hidden">
                         <DialogTitle className="hidden"></DialogTitle>
                         <DialogDescription className="hidden">hello</DialogDescription>
@@ -297,6 +107,9 @@ const SigninModal = () => {
 
                         {/* Login Form */}
                         <TabsContent value="login">
+                            <div className={"mb-4 text-lg text-center font-inter font-semibold "}>
+                                Login to your account
+                            </div>
                             <form id="form-login" onSubmit={loginForm.handleSubmit(onLogin)}>
                                 <FieldGroup className="gap-4">
                                     <Controller
@@ -337,10 +150,8 @@ const SigninModal = () => {
                             </form>
                             <div className="flex flex-col! gap-2 font-inter mt-4">
                                 <div className="flex gap-2 w-full">
-                                    <DialogClose className="w-full">
-                                        <Button variant="outline" className="cursor-pointer w-full">
+                                    <DialogClose className="w-full cursor-pointer text-sm">
                                             Cancel
-                                        </Button>
                                     </DialogClose>
                                     <Button form="form-login" type="submit" className="cursor-pointer w-full">
                                         Login
@@ -351,6 +162,9 @@ const SigninModal = () => {
 
                         {/* Signup Form */}
                         <TabsContent value="signup">
+                            <div className={"mb-4 text-lg text-center font-inter font-semibold "}>
+                                Create New Account
+                            </div>
                             <form
                                 id="form-signup"
                                 onSubmit={signupForm.handleSubmit(onSignup)}
@@ -506,10 +320,8 @@ const SigninModal = () => {
 
                             <div className="flex flex-col! gap-2 font-inter mt-4">
                                 <div className="flex gap-2 w-full">
-                                    <DialogClose className="w-full">
-                                        <Button variant="outline" className="cursor-pointer w-full">
-                                            Cancel
-                                        </Button>
+                                    <DialogClose className="w-full cursor-pointer text-sm">
+                                        Cancel
                                     </DialogClose>
                                     <Button form="form-signup" type="submit" className="cursor-pointer w-full">
                                         Signup
