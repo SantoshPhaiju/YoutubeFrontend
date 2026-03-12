89d7942ccc8739821c6c3cae167eb91a50df5d2d
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-12 21:13:06 +0545

    feat: add cookies for token management and configure axios with auth header
    - Added `cookies-next` and `js-cookie` dependencies for handling cookies.
    - Configured axios to include `Authorization` header using `accessToken` from cookies.

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,21 @@
 import axios from "axios";
+import {getCookie} from "cookies-next";
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
 });
+
+
+api.interceptors.request.use(
+    (config) => {
+        const token = getCookie('accessToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+);
 
 export default api;
