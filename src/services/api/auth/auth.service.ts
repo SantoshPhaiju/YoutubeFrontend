b67226495446a4c7b7e52a78ab1ad1ec17b45c13
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-15 21:55:23 +0545

    refactor: update `signIn` and `signUp` services for improved payload handling
    - Replaced individual parameters in `signIn` with a consolidated `payload`, supporting `usernameoremail`.
    - Simplified `signUp` service to directly send `FormData` without additional headers.

--- src/services/api/auth/auth.service.ts
+++ src/services/api/auth/auth.service.ts
@@ -3,18 +3,12 @@
 import api from "@/services/axios";
 import {IApiResponse} from "@/@types/api-response.type";
 
-export const signIn = async ({
-                                 email,
-                                 password,
-                             }: {
-    email: string;
+export const signIn = async (payload: {
+    usernameoremail: string;
     password: string;
 }) => {
     try {
-        const response = await api.post("/users/login", {
-            email,
-            password,
-        });
+        const response = await api.post("/users/login", payload);
         return response.data;
     } catch (error) {
         throw error;
@@ -25,14 +19,12 @@ export const signUp = async ({
                                  formData
                              }: {
     formData: FormData;
-}) => {
+}): Promise<IApiResponse<any>> => {
+    for (const [key, value] of formData.entries()) {
+        console.log(`${key}: ${value} in formData`);
+    }
     try {
-        const response = await api.post("/users/register", {
-            formData,
-            headers: {
-                "Content-Type": "multipart/form-data"
-            }
-        });
+        const response = await api.post("/users/register", formData);
         return response.data;
     } catch (error) {
         throw error;
