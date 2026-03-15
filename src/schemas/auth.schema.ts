296483483f22e128ee6afe5cbd1512b16a71b4b4
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-15 21:54:50 +0545

    feat: update auth schemas to enhance validation and support file inputs
    - Modified `loginFormSchema` to accept either username or email and set minimum password length.
    - Updated `signupFormSchema` to use `File` for avatar input and improve validation rules.

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,9 +1,10 @@
 import {z} from "zod";
 
-export const loginFormSchema = z.object({
-    email: z.string().email("Invalid email address"),
-    password: z.string(),
-})
+export const loginFormSchema = z
+    .object({
+        usernameoremail: z.string().min(3),
+        password: z.string().min(6, "Password must be at least 6 characters"),
+    });
 
 export const signupFormSchema = z.object({
     username: z.string().min(3, "Username must be at least 3 characters"),
@@ -11,5 +12,5 @@ export const signupFormSchema = z.object({
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters"),
     confirmPassword: z.string().min(6, "Confirm password must be at least 6 characters"),
-    avatar: z.string().optional(),
-})
+    avatar: z.instanceof(File).nullable().optional(),
+});
