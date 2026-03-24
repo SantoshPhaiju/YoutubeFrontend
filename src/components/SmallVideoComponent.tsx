89d0083ac2c506dae3cfad3a21b441d9dd3c880e
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-24 14:28:50 +0545

    Updated small video component to use same video size for all the occurrences and made dynamic to use video prop.

--- src/components/SmallVideoComponent.tsx
+++ src/components/SmallVideoComponent.tsx
@@ -2,50 +2,55 @@ import Image from "next/image";
 import Link from "next/link";
 import { GoKebabHorizontal } from "react-icons/go";
 import { cn } from "@/lib/utils";
+import {timeAgo} from "@/utils/timeAgo";
 
 const SmallVideoComponent = (
     {
-        videoSize = "small",
-    }: { videoSize?: "small" | "medium" | "large" }
+        video
+    }: { video: any }
 ) => {
     return (
-        <div className={cn(`h-auto aspect-video cursor-pointer`, {
-            "min-h-[140px] w-auto": videoSize === "small",
-            "aspect-video": videoSize === "medium",
-            "min-h-[250px] w-[320px]": videoSize === "large",
-        })}>
+        <div className={cn(`video w-full cursor-pointer`)}>
             <div className="thumbnail">
-                <Link href="/watch?v=123">
+                <Link href={`/watch?v=${video?._id}`}>
                     <Image
-                        src={"/assets/thumb.jpg"}
+                        src={video?.thumbnail || `/assets/thumb.jpg`}
                         height={200}
                         width={300}
-                        className={cn(`h-auto min-h-[140px] w-full rounded-md object-cover`)}
+                        className={cn(`aspect-video rounded-md object-cover w-full`)}
                         alt="VideoComponent"
                     />
                 </Link>
             </div>
 
-            <div className="details py-1 pt-2 flex">
-                <div className="flex flex-col gap-1 px-1">
-                    <div className="title text-sm text-black line-clamp-2">
-                        How to Make Money Using Google Maps in 2025 ($100 - $200 PER DAY)
-                        something
+            <div className="details py-1 pt-2.5 flex">
+                <div
+                    className="flex flex-col gap-0.5 px-1 w-full">
+                    <div
+                        className="title text-sm text-black font-medium line-clamp-2 w-[98%]">
+                        {video?.title || "Video Title"}
                     </div>
-                    <div className="flex flex-col mt-px">
+                    <div
+                        className="flex flex-col w-full">
                         {/*<div className="channelName text-sm text-gray-600 flex justify-start items-center gap-1">*/}
                         {/*    Santosh Phaiju <MdVerified />*/}
                         {/*</div>*/}
-                        <div className="flex justify-start items-center gap-2 text-gray-600 text-sm">
-                            <div className="views">99k views</div>
-                            <div className="dot h-1 w-1 bg-black rounded-full"></div>
-
-                            <div className="publishedDate">3 days ago</div>
+                        <div
+                            className="flex justify-start items-center gap-2 text-gray-600 text-xs">
+                            <div
+                                className="views">{video?.viewCount || 0} views
+                            </div>
+                            <div
+                                className="dot h-1 w-1 bg-black rounded-full"></div>
+                            <div className="publishedDate">
+                                {timeAgo(video?.createdAt || new Date())}
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="threeDot h-full">
-                    <GoKebabHorizontal className={"rotate-90 text-xl"} />
+                    <GoKebabHorizontal
+                        className={"rotate-90 text-xl"}/>
                 </div>
             </div>
         </div>
